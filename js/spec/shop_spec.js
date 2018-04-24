81fa22439e443d4638adb23807f24834f9900c2e
danielcampos <ddregalo@gmail.com>
2018-04-24 18:49:47 +0100

    Write shop spec test - has items property that is array

--- js/spec/shop_spec.js
+++ js/spec/shop_spec.js
@@ -1,5 +1,10 @@
 describe("Shop", function() {
 
+  it("should have an items property that is empty array", function() {
+    let gildedRose = new Shop();
+    expect(gildedRose.items).toEqual([]);
+  });
+
   it("should return the item name that was updated", function() {
     let gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     let items = gildedRose.updateQuality();
